3c36aede0fc35926334f5203f27bf8fbfa1ec4df
sevvalbayrak <svvlerdenk@gmail.com>
2023-06-15 18:03:32 +0300

    success

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from "react";
 import { Route, Switch } from "react-router";
-import Home from "./components/Home Component/Home"
-import Orders from "./components/Orders Component/Orders"
-import './App.css'
+import Home from "./components/Home Component/Home";
+import Orders from "./components/Orders Component/Orders";
+import Success from "./components/Success Component/Success";
+import "./App.css";
 
 const App = () => {
   return (
     <Switch>
-    <Route exact path="/">
-      <Orders />
-    </Route>
-    {/* <Route path="/about">
+      <Route exact path="/">
+        <Success />
+      </Route>
+      {/* <Route path="/success">
     </Route>
     <Route path="/:user">
     </Route>
     <Route>
     </Route> */}
-  </Switch>
-    
+    </Switch>
   );
 };
 export default App;
---
+++ src/components/Success Component/Success.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./success.css";
+
+const Success = () => {
+  return (
+    <div class="success-wrapper flex-center-column">
+      <div class="successContent-wrapper">
+        <h1 class="slogan">Teknolojik Yemekler</h1>
+        <div class="textSuccess-wrapper  flex-center-column">
+          <p class="successTitle">
+            TEBRİKLER! <br />
+            SİPARİŞİNİZ ALINDI!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Success;
